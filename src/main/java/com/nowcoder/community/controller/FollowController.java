a2066dfc1613f070b56f619fb1cef909de50665a
Robin Liu <robinliu.lloii@gmail.com>
2022-06-16 15:00:21 +0800

    feat: Redis关注 & 取消关注

---
+++ src/main/java/com/nowcoder/community/controller/FollowController.java
@@ -0,0 +1,45 @@
+package com.nowcoder.community.controller;
+
+import com.nowcoder.community.annotation.LoginRequired;
+import com.nowcoder.community.entity.User;
+import com.nowcoder.community.service.FollowService;
+import com.nowcoder.community.util.CommunityConstant;
+import com.nowcoder.community.util.CommunityUtil;
+import com.nowcoder.community.util.HostHolder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class FollowController implements CommunityConstant {
+
+    @Autowired
+    private FollowService followService;
+
+    @Autowired
+    private HostHolder hostHolder;
+
+    @LoginRequired
+    @PostMapping("/follow")
+    @ResponseBody
+    public String follow(int entityType, int entityId) {
+        User user = hostHolder.getUser();
+
+        followService.follow(user.getId(), entityType, entityId);
+
+        return CommunityUtil.getJSONString(0, "已关注!");
+    }
+
+    @LoginRequired
+    @PostMapping("/unfollow")
+    @ResponseBody
+    public String unFollow(int entityType, int entityId) {
+        User user = hostHolder.getUser();
+
+        followService.unFollow(user.getId(), entityType, entityId);
+
+        return CommunityUtil.getJSONString(0, "已取消关注!");
+    }
+
+}
--- src/main/java/com/nowcoder/community/util/RedisKeyUtil.java
+++ src/main/java/com/nowcoder/community/util/RedisKeyUtil.java
@@ -5,6 +5,8 @@ public class RedisKeyUtil {
     private static final String SPLIT = ":";
     private static final String PREFIX_ENTITY_LIKE = "like:entity";
     private static final String PREFIX_USER_LIKE = "like:user";
+    private static final String PREFIX_FOLLOWEE = "followee"; // 被关注者 用户/帖子/题目
+    private static final String PREFIX_FOLLOWER = "follower"; // 粉丝
 
     // 某个实体的赞
     // like:entity:entityType:entityId -> set(userId)
@@ -17,6 +19,20 @@ public class RedisKeyUtil {
     // like:user:userId -> int
     public static String getUserLikeKey(int userId) {
         return PREFIX_USER_LIKE + SPLIT + userId;
+    }
+
+    // 某个用户关注的实体 以当前时间作为排序
+    // followee:userId:entityType -> zset(entityId, now)
+    // getFolloweeKey: followee标识关注实体 userId:entityType表示某一用户关注的类型
+    // value:          entityId now 关注的实体id 关注时间
+    public static String getFolloweeKey(int userId, int entityType) {
+        return PREFIX_FOLLOWEE + SPLIT + userId + SPLIT + entityType;
+    }
+
+    // 某个实体拥有的粉丝
+    // follower:entityType:entityId -> zset(userId, now)
+    public static String getFollowerKey(int entityType, int entityId) {
+        return PREFIX_FOLLOWER + SPLIT + entityType + SPLIT + entityId;
     }
 
 }
