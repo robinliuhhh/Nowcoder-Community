f5b55446177262461d3913959677a22f9cd6f65e
Robin Liu <robinliu.lloii@gmail.com>
2022-06-22 19:26:17 +0800

    feat: 用户头像上传至阿里云OSS

--- src/main/java/com/nowcoder/community/controller/UserController.java
+++ src/main/java/com/nowcoder/community/controller/UserController.java
@@ -1,5 +1,8 @@
 package com.nowcoder.community.controller;
 
+import com.aliyun.oss.OSS;
+import com.aliyun.oss.OSSClientBuilder;
+import com.aliyun.oss.model.PutObjectRequest;
 import com.nowcoder.community.annotation.LoginRequired;
 import com.nowcoder.community.entity.User;
 import com.nowcoder.community.service.FollowService;
@@ -15,11 +18,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletResponse;
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.OutputStream;
@@ -38,6 +43,21 @@ public class UserController implements CommunityConstant {
 
     @Value("${server.servlet.context-path}")
     private String contextPath;
+
+    @Value("${aliyun.endpoint}")
+    private String endpoint;
+
+    @Value("${aliyun.access-key-id}")
+    private String accessKeyId;
+
+    @Value("${aliyun.access-key-secret}")
+    private String accessKeySecret;
+
+    @Value("${aliyun.bucket-name}")
+    private String bucketName;
+
+    @Value("${aliyun.bucket-header-dir}")
+    private String headerDir;
 
     @Autowired
     private UserService userService;
@@ -58,8 +78,8 @@ public class UserController implements CommunityConstant {
     }
 
     @LoginRequired
-    @PostMapping("/upload")
-    public String uploadHeader(MultipartFile headerImage, Model model) {
+    @PostMapping("/upload-header")
+    public String uploadHeaderToAliyun(MultipartFile headerImage, Model model) {
         if (headerImage == null) {
             model.addAttribute("error", "您还没有选择图片!");
             return "/site/setting";
@@ -73,25 +93,66 @@ public class UserController implements CommunityConstant {
         }
 
         // 生成随机文件名
-        fileName = CommunityUtil.generateUUID() + suffix;
-        // 确定文件存放的路径（dest目标路径）
-        File dest = new File(uploadPath + "/" + fileName);
+        fileName = headerDir + "/" + CommunityUtil.generateUUID() + suffix;
+        // 创建OSSClient实例
+        OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
         try {
-            // 存储文件
-            headerImage.transferTo(dest);
+            PutObjectRequest putObjectRequest = new PutObjectRequest(bucketName, fileName, headerImage.getInputStream());
+            // 上传文件
+            ossClient.putObject(putObjectRequest);
         } catch (IOException e) {
-            logger.error("上传文件失败: " + e.getMessage());
-            throw new RuntimeException("上传文件失败,服务器发生异常!", e);
+            logger.error("上传文件失败:" + e.getMessage());
+        } finally {
+            if (ossClient != null) {
+                ossClient.shutdown();
+            }
         }
 
         // 更新当前用户的头像的路径（web访问路径）
-        // http://localhost:8080/community/user/header/xxx.png
+        // https://robins-community.oss-cn-shanghai.aliyuncs.com/header/da394fbb73b147f99b4164f9f6d8f54c.png
         User user = hostHolder.getUser();
-        String headerUrl = domain + contextPath + "/user/header/" + fileName;
+        String headerUrl = "https://" + bucketName + "." + endpoint + "/" + fileName;
         userService.updateHeader(user.getId(), headerUrl);
 
         return "redirect:/index";
     }
+
+//    @Deprecated // 上传至阿里云OSS 此方法上传至本地 故废弃
+//    @LoginRequired
+//    @PostMapping("/upload")
+//    public String uploadHeader(MultipartFile headerImage, Model model) {
+//        if (headerImage == null) {
+//            model.addAttribute("error", "您还没有选择图片!");
+//            return "/site/setting";
+//        }
+//
+//        String fileName = headerImage.getOriginalFilename();
+//        String suffix = fileName.substring(fileName.lastIndexOf("."));
+//        if (StringUtils.isBlank(suffix)) {
+//            model.addAttribute("error", "文件的格式不正确!");
+//            return "/site/setting";
+//        }
+//
+//        // 生成随机文件名
+//        fileName = CommunityUtil.generateUUID() + suffix;
+//        // 确定文件存放的路径（dest目标路径）
+//        File dest = new File(uploadPath + "/" + fileName);
+//        try {
+//            // 存储文件
+//            headerImage.transferTo(dest);
+//        } catch (IOException e) {
+//            logger.error("上传文件失败: " + e.getMessage());
+//            throw new RuntimeException("上传文件失败,服务器发生异常!", e);
+//        }
+//
+//        // 更新当前用户的头像的路径（web访问路径）
+//        // http://localhost:8080/community/user/header/xxx.png
+//        User user = hostHolder.getUser();
+//        String headerUrl = domain + contextPath + "/user/header/" + fileName;
+//        userService.updateHeader(user.getId(), headerUrl);
+//
+//        return "redirect:/index";
+//    }
 
     // 不需要@LoginRequired 因为不登录也可以看别人的头像
     // 图片是二进制流输出 通过HttpServletResponse直接输出 所以返回值是void
@@ -109,7 +170,7 @@ public class UserController implements CommunityConstant {
         ) {
             // byte[1024] 一批一批输出 效率高
             byte[] buffer = new byte[1024];
-            int b = 0;
+            int b;
             while ((b = fis.read(buffer)) != -1) {
                 os.write(buffer, 0, b);
             }
